8e5ec73883b2d4c97bc6e015d86808207508757e
juan.goez <juan.goez@enlared.net.co>
2019-11-05 15:19:00 -0500

    recto3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { counter: 0 }
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.onChangeText}></textarea>
+        <div className="counter">{this.state.counter}</div>
       </div>
     );
+  }
+
+  onChangeText = (e) => {
+    const length = e.target.value.length
+    this.setState({
+      counter: length
+    });
   }
 }
 
